516cd540edd031dc1b91edf15f41991b2ab52753
Robin Zigmond <robinzig@hotmail.com>
2017-08-11 03:00:01 +0100

    crude implementation of powerups (and powerdowns!) - at the moment they appear as text on white boxes. All functionality seems to work though.

--- breakout.js
+++ breakout.js
@@ -7,7 +7,7 @@ const ctx = canvas.getContext("2d");
 const width = 1000;
 const height = 500;
 
-var timeRemaining, time, prevTime;
+var timeRemaining, time, prevTime, currentLevel;
 
 var paddle = {
     width: 80,
@@ -43,6 +43,10 @@ ball.normaliseAngle = function(angle) {
     return angle;
 }
 
+var powerups = [];
+var powerupTypes = ["+time", "-time", "+paddle", "-paddle", "+speed",
+    "-speed", "+ball", "-ball"];
+
 $("body").keydown(function(e) {
     if (e.keyCode == 39) {
         e.preventDefault();
@@ -76,7 +80,7 @@ function makeBlocks() {
                     // extend previous block's width
                     blocks.data[blocks.data.length-1].width += blocks.unitWidth;
                 }
-                else {
+                else if (entry != 9) { // 9 will code for a powerup-producing block
                     // add new block
                     blocks.data.push({
                         xPos: blocks.unitWidth*colNo,
@@ -85,6 +89,16 @@ function makeBlocks() {
                         height: blocks.rowHeight,
                         colour: colours[entry-1],
                         stillThere: true // used later to track blocks hit by the ball
+                    });
+                }
+                else { // power-up block, they will be coloured WHITE
+                    blocks.data.push({
+                        xPos: blocks.unitWidth*colNo,
+                        yPos: blocks.rowHeight*rowNo + blocks.heightOffset,
+                        width: blocks.unitWidth,
+                        height: blocks.rowHeight,
+                        colour: "white",
+                        stillThere: true
                     });
                 }
             }
@@ -103,9 +117,12 @@ function initialise() {
     ball.speed = 4;
     ball.angle = Math.PI/2;
     ball.active = false;
+    paddle.width = 80;
+    paddle.speed = 5;
     paddle.xPos = paddle.startXPos;
     paddle.goingEast = paddle.goingWest = false;
     makeBlocks();
+    powerups = [];
     running = true;
     timeRemaining = (levels[currentLevel-1].time+1)*1000;  // add extra second to get starting time displayed 
                                                            // correctly. It is taken off at the end!
@@ -158,6 +175,15 @@ function drawStuff() {
         ctx.fillRect(block.xPos, block.yPos, block.width, block.height);
         ctx.strokeRect(block.xPos, block.yPos, block.width, block.height);
     });
+
+    // draw powerups. For now they will be small white rectangles with text on. Later I hope to draw something!
+    powerups.forEach(function(powerup) {
+        ctx.fillStyle = "white";
+        ctx.fillRect(powerup.xPos, powerup.yPos, powerup.width, powerup.height);
+        ctx.font = "14px Arial";
+        ctx.fillStyle = "black";
+        ctx.fillText(powerup.text, powerup.xPos, powerup.yPos);
+    });
 }
 
 
@@ -184,12 +210,66 @@ function moveStuff() {
     else {
         ball.centre.xPos = paddle.xPos + paddle.width/2; // ball needs to stick to paddle!
     }
-    
+
+    // powerups, fall when on screen:
+    powerups.forEach(function(powerup) {
+        powerup.yPos += powerup.speed;
+    });
 }
 
 
 function hitDetection() {
     var hitBlockBelow, hitBlockAbove, hitBlockLeft, hitBlockRight;
+
+    // first handle powerups!
+    powerups.forEach(function(powerup) {
+        // if hitting paddle, they disappear and take effect!
+        if (powerup.xPos+powerup.width>=paddle.xPos && powerup.xPos<=paddle.xPos+paddle.width
+            && powerup.yPos+powerup.height>=paddle.yPos) {
+                powerup.stillThere = false;
+                if (powerup.text == "+time") {
+                    timeRemaining += 60000;
+                }
+                else if (powerup.text == "-time") {
+                    timeRemaining = Math.max(timeRemaining-60000, 1000);
+                }
+                else if (powerup.text == "+paddle") {
+                    // make sure the expansion keeps the same centre, not the same left boundary!
+                    oldWidth = paddle.width;
+                    oldXPos = paddle.xPos;
+                    paddle.width = Math.min(paddle.width+20, width);
+                    paddle.xPos = (oldXPos+oldWidth/2)-paddle.width/2;
+                    // paddle movement, below, will move it as needed to fit on the screen!
+                }
+                else if (powerup.text == "-paddle") {
+                    oldWidth = paddle.width;
+                    oldXPos = paddle.xPos;
+                    paddle.width = Math.max(paddle.width-20, 20);
+                    paddle.xPos = (oldXPos+oldWidth/2)-paddle.width/2;
+                }
+                else if (powerup.text == "+speed") {
+                    paddle.speed += 1;
+                }
+                else if (powerup.text == "-speed") {
+                    paddle.speed = Math.max(paddle.speed-1, 1);
+                }
+                else if (powerup.text == "+ball") {
+                    ball.speed += 1;
+                }
+                else if (powerup.text == "-ball") {
+                    ball.speed = Math.max(ball.speed-1, 1);
+                }
+        }
+        // just disappear without effect if it hits the bottom:
+        else if (powerup.yPos+powerup.height>=height) {
+            powerup.stillThere = false;
+        }
+    });
+    // update powerups array by removing ones that are no longer there:
+    powerups = powerups.filter(powerup => powerup.stillThere);
+
+    // next deal with paddle
+
     // paddle can't move beyond right/left walls
     if (paddle.xPos<=0) {
         paddle.xPos = 0;
@@ -259,6 +339,18 @@ function hitDetection() {
         ball.centre.yPos-ball.radius<=block.yPos+block.height) {
             // first make sure the block is removed from the screen!
             block.stillThere = false;
+            // then generate a new powerup if the block was white:
+            if (block.colour == "white") {
+                powerups.push({
+                    xPos: (block.xPos+(block.width-blocks.unitWidth)/2),
+                    yPos: block.yPos,
+                    width: blocks.unitWidth,
+                    height: blocks.rowHeight,
+                    speed: 2,
+                    text: powerupTypes[Math.floor(powerupTypes.length*Math.random())],
+                    stillThere: true
+                });
+            }
             // now work out new angle for ball. It depends on whether it hit the block from above/below
             // or left/right.
             if ((ball.centre.yPos<block.yPos && !hitBlockAbove)
